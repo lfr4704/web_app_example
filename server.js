ec1b36381ff89061e97b932645857e94c8ad4715
Luis <lfr4704@gmail.com>
2018-04-02 23:02:03 -0700

    revised

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-//create package jsaon to save all dependencies (npm init --yes) to create default package
-//then install express.js dependency (npm install -s express) "-s" saves the dependency to json package
+//create package jsaon to save all dependencies first by typing the following in command line: (npm init --yes) to create default package
+//then install express.js dependency (npm install -S express) or (npm install express --save) "-S" saves the dependency to json package
+//then create gitignore file. refer to this link for how to create file in windows https://stackoverflow.com/questions/10744305/how-to-create-gitignore-file
 
 let express = require('express');
-let app = express();
+let app = express();  //this sets reference to an instance of express
 
 app.use(express.static(__dirname));
 
-var messages = [
+let messages = [
 { name: 'Tim', message: 'Hi'},
 { name: 'Jane', message: 'Hello'}
-]
+];
 
 
 app.get('/messages', (req, res) =>{
-	res.send('messages!')
+	res.send(messages);
 });
 
 
-
+//for windows to run "localhost:3000/messages" the server.js needs be running in the command line "node server.js"
 let server = app.listen(3000, () => {
     
-    console.log("server is listening on port", server.address().port)
+    console.log("server is listening on port", server.address().port);
 });
